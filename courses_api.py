d7c700409a1b311e05c5374a2266ac38a6d3c13c
Terry Kim <terrykim950@gmail.com>
2021-04-18 10:19:36 -0400

    cleaning data

--- courses_api.py
+++ courses_api.py
@@ -1,4 +1,20 @@
 import requests
+import json
 
-courses = requests.get('http://coursews.mit.edu/coursews/?term=2021SP').text
-print(courses)
+courses = requests.get('http://coursews.mit.edu/coursews/?term=2021SP').json()
+math = []
+cs = []
+business = []
+
+for course in courses["items"]:
+    if course["type"] == "Class":
+        if "18." in course["id"]:
+            math.append(course)
+        elif "6." in course["id"]:
+            cs.append(course)
+        elif "15." in course["id"]:
+            business.append(course)
+
+# type, id, total-units, prereqs, semester,
+# input: list of classes taken in spring semester
+# output: time of the classes

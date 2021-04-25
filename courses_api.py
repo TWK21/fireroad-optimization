7c44d4d3f308d20fcf6ec17fdb161c6a28ab4c83
Terry Kim <terrykim950@gmail.com>
2021-04-25 10:58:13 -0400

    added list of all courses

--- courses_api.py
+++ courses_api.py
@@ -18,3 +18,7 @@ for course in courses["items"]:
 # type, id, total-units, prereqs, semester,
 # input: list of classes taken in spring semester
 # output: time of the classes
+
+
+# /recommend/get (GET)
+# Takes an optional parameter t indicating the type of recommendation to return. Returns a dictionary of recommendation types mapped to JSON strings indicating the recommended subjects and their rating values.

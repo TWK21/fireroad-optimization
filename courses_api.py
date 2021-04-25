871d03a03789e2a05683662b465b2db13ff94f7b
Terry Kim <terrykim950@gmail.com>
2021-04-25 11:49:00 -0400

    fetched and cleaned courses data

--- courses_api.py
+++ courses_api.py
@@ -1,23 +1,40 @@
 import requests
 import json
+import pandas as pd
 
-courses = requests.get('http://coursews.mit.edu/coursews/?term=2021SP').json()
-math = []
-cs = []
-business = []
+# fetching courses from MIT coursews API
+c = requests.get('http://coursews.mit.edu/coursews/?term=2021SP').json()
 
-for course in courses["items"]:
-    if course["type"] == "Class":
-        if "18." in course["id"]:
-            math.append(course)
-        elif "6." in course["id"]:
-            cs.append(course)
-        elif "15." in course["id"]:
-            business.append(course)
+# initializing data structures
+c = c['items']
+ID = []
+Units = []
+Course = []
+Prereqs = []
+Semester = []
 
-# type, id, total-units, prereqs, semester,
-# input: list of classes taken in spring semester
-# output: time of the classes
+# cleaning data from the API
+for course in (c):
+    if course['type'] == 'Class':
+        ID.append(course["id"])
+        Units.append(course['total-units'])
+        Prereqs.append(course['prereqs'])
+        Semester.append(course['semester'])
+        Course.append(course["course"])
+
+# exporting the data to csv
+courses = pd.DataFrame({'ID': ID, "Units": Units, "Course": Course,
+                        "Prereqs": Prereqs, "Semester": Semester})
+datatoexcel = pd.ExcelWriter("test2.xlsx", engine="xlsxwriter")
+courses.to_excel(datatoexcel, sheet_name="Sheet1")
+datatoexcel.save()
+
+
+# 1. change xlsx file to csv
+# 2. use term 2021FA to add all the coruses only offered in the fall
+# 3. fetch rating from fireroad API or randomly generate utility
+# 4. add columns for isGIR and isHASS {1,0}
+# 5. add a column for time for scheduling the current semester
 
 
 # /recommend/get (GET)
